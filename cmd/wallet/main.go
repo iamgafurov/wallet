f809c8cd8facced37b2859f118dae038c8113a8b
Ravshan <iamgafurov@gmail.com>
2020-10-02 18:00:06 +0500

    v1

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -8,10 +8,10 @@ import (
 func main(){
 	srv := &wallet.Service{}
 	srv.RegisterAccount(types.Phone("90999390"))
-	account,err := srv.FindAccoutByID(1)
+	account,err := srv.FindAccountByID(1)
 	if err != nil {
 		fmt.Println(err)
 	}else {
-		fmt.Println(*account)
+		fmt.Println(account)
 	}
 }
