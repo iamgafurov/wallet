98eb251bbc29e1527d94723176e0d98636b34ba9
Ravshan <iamgafurov@gmail.com>
2020-10-02 18:00:49 +0500

    v1

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -7,30 +7,34 @@ import (
 
 
 var ErrAccountNotFound = errors.New("account not found")
+var ErrPhoneExist = errors.New("account by this phone exist")
+
 type Service struct {
-	accounts []types.Account
-	payments []types.Payment
+	accounts []*types.Account
+	payments []*types.Payment
 }
 
-func (s *Service) RegisterAccount(phone types.Phone){
+func (s *Service) RegisterAccount(phone types.Phone)(*types.Account,error){
 	nextID := int64(0)
 	for _,account := range s.accounts{
 		if account.Phone == phone{
-			return
+			return nil, ErrPhoneExist
 		}
 	}
 	nextID++
-	s.accounts = append(s.accounts, types.Account{
+	newAccount := &types.Account{
 		ID: nextID,
 		Phone: phone,
 		Balance: 0,
-	})
+	}
+	s.accounts = append(s.accounts,newAccount)
+	return newAccount,nil
 }
 
-func (s *Service) FindAccoutByID(accountID int64)(*types.Account,error){
+func (s *Service) FindAccountByID(accountID int64)(*types.Account,error){
 	for _,account := range s.accounts {
 		if (account).ID == accountID {
-			return &account,nil
+			return account,nil
 		}
 	}
 	return nil, ErrAccountNotFound
